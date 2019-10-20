981929b648341fe3bc5fdc1b04f79d780efaf115
Derek-M-Davis <52020929+Derek-M-Davis@users.noreply.github.com>
2019-10-20 11:57:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,27 +6,29 @@ $(() => {
     // var url = 'https://api-v3.igdb.com/achievements';
     // var xhr = createCORSRequest('GET', url);
     // xhr.send();
-    $.ajax({
-      GET: 'https://api-v3.igdb.com/games/',
-      Headers: {
-        'Access-Control-Allow-Credential': true,
-        'Accept': 'application/json',
-          'user-key': 'e9e6341303be555f38f64c89b5928eaf',
-      },
-    //   data: "fields achievement_icon,category,created_at,description,external_id,game,language,locked_achievement_icon,name,owners,owners_percentage,rank,slug,tags,updated_at;"
-    })
-      .then(response => {
-          console.log(response.data);
-      })
-      .catch(err => {
-          console.error(err);
-      });
+//     $.ajax({
+//       url: 'https://api-v3.igdb.com/games/',
+//       Headers: {
+//         'Access-Control-Allow-Credential': true,
+//         'Accept': 'application/json',
+//           'user-key': 'e9e6341303be555f38f64c89b5928eaf',
+//       },
+//     //   data: "fields achievement_icon,category,created_at,description,external_id,game,language,locked_achievement_icon,name,owners,owners_percentage,rank,slug,tags,updated_at;"
+//     })
+//       .then(response => {
+//           console.log(response.data);
+//       })
+//       .catch(err => {
+//           console.error(err);
+//       });
     
 
-
-
-
-
+    $.ajax({
+         url: "https://api-v3.igdb.com/games/",
+         type: "GET",
+         beforeSend: function(xhr){xhr.setRequestHeader('X-Test-Header', 'test-value');},
+         success: function() { console.log('got data') }
+      });
 
 
 
