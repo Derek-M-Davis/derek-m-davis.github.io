15a2f1177814edaff9aca1372d2dd625683ddd59
Derek-M-Davis <52020929+Derek-M-Davis@users.noreply.github.com>
2019-10-20 12:13:18 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(() => {
+(() => {
     
 
 
@@ -22,13 +22,21 @@ $(() => {
 //           console.error(err);
 //       });
     
-
-    $.ajax({
-         url: "https://api-v3.igdb.com/games/",
-         type: "GET",
-         beforeSend: function(xhr){xhr.setRequestHeader('X-Test-Header', 'test-value');},
-         success: function() { console.log('got data') }
-      });
+axios({
+  url: "https://api-v3.igdb.com/games",
+  method: 'GET',
+  headers: {
+      'Accept': 'application/json',
+      'user-key': 'e9e6341303be555f38f64c89b5928eaf',
+  },
+//   data: "fields achievement_icon,category,created_at,description,external_id,game,language,locked_achievement_icon,name,owners,owners_percentage,rank,slug,tags,updated_at;"
+})
+  .then(response => {
+      console.log(response.data);
+  })
+  .catch(err => {
+      console.error(err);
+  });
 
 
 
