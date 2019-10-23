12eeb796a39c535612b869a8f353de1f40e13375
Derek <derekdavis408@gmail.com>
2019-10-22 21:17:02 -0700

    final formtatting

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ $(() => {
     $closeBtn.on('click', closeModal)
 
 
-  // OMDB api input
+  // OMDB api input user input
   $('form').on('submit', (event) => {
     event.preventDefault()
 
@@ -46,4 +46,33 @@ $(() => {
     )
     $('form').trigger('reset');
  })
+   
+    // OMDB api input Random input BROKEN!!!!!! ugh
+    $('button').on('click', (event) => {
+      event.preventDefault()
+      const randNum = Math.floor(Math.random() * 9000000);
+        console.log(randNum)
+      $.ajax({
+        url: "https://www.omdbapi.com/?apikey=c6a3e6b0&i=tt" + randNum,
+      }).then(
+        (data) => {
+          $('#title').html(data.Title)
+          $('#year').html(data.Year)
+          $('#rated').html(data.Rated)
+          $('#poster').attr('src', data.Poster)
+          $('#releaseDate').html(data.Released)
+          $('#awards').html(data.Awards)
+          $('#boxOffice').html(data.BoxOffice)
+          $('#plot').html(data.Plot)
+          $('#actors').html(data.Actors)
+            console.log(data)
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+    })
+
+
+
 })
