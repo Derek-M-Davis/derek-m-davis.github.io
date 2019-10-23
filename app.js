61ebed34a75fc9eaccd5e038beb5ffbaaae47ab0
Derek <derekdavis408@gmail.com>
2019-10-22 17:26:46 -0700

    changes to flow of code

--- app.js
+++ app.js
@@ -1,55 +1,49 @@
 // console.log($)
 
-
-
-
 $(() => {
+    
+  // setting objects
     const $openBtn = $('#openModal')
-
     const $modal = $('#modal')
-  
     const $closeBtn = $('#close')
-  
+  // functions for objects and click functions
     const openModal = () => {
       $modal.css('display', 'block')
     }
-  
     $openBtn.on('click', openModal)
-  
+    
     const closeModal = () => {
       $modal.css('display', 'none')
     }
-  
     $closeBtn.on('click', closeModal)
 
 
-
+  // OMDB api input
   $('form').on('submit', (event) => {
     event.preventDefault()
 
-  const userInput = $('input[type="text"]'). val()
+    const userInput = $('input[type="text"]'). val()
 
-  $.ajax({
-    url: "http://www.omdbapi.com/?apikey=c6a3e6b0&t=" + userInput,
-  }).then(
-    (data) => {
-      $('#title').html(data.Title)
-      $('#year').html(data.Year)
-      $('#rated').html(data.Rated)
-      $('#poster').attr('src', data.Poster)
-      $('#releaseDate').html(data.Released)
-      $('#awards').html(data.Awards)
-      $('#boxOffice').html(data.BoxOffice)
-      $('#plot').html(data.Plot)
-      $('#actors').html(data.Actors)
-
-
-      console.log(data)
-    },
-    (error) => {
-      console.log(error)
-    }
-  )
+    $.ajax({
+      url: "http://www.omdbapi.com/?apikey=c6a3e6b0&t=" + userInput,
+    }).then(
+      (data) => {
+        $('#title').html(data.Title)
+        $('#year').html(data.Year)
+        $('#rated').html(data.Rated)
+        $('#poster').attr('src', data.Poster)
+        $('#releaseDate').html(data.Released)
+        $('#awards').html(data.Awards)
+        $('#boxOffice').html(data.BoxOffice)
+        $('#plot').html(data.Plot)
+        $('#actors').html(data.Actors)
+        
+        console.log(data)
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
     $('form').trigger('reset');
-})
+ })
 })
